5254283b83e30291ded2e1f6dfc8219c1a26c32c
racharlasrikanth <racharlasrikanth3211@gmail.com>
2020-10-05 11:15:34 +0530

    added-styles to the page

--- src/App.js
+++ src/App.js
@@ -13,9 +13,16 @@ function App() {
   return (
     <div className="App">
       <div className="content">
-        <h1>Counter {counter}</h1>
-        <button onClick={() => dispatch(increment(5))}>+</button>
-        <button onClick={() => dispatch(decrement(2))}>-</button>
+        <h1>Counter: {counter}</h1>
+        <button className="button-add" onClick={() => dispatch(increment(5))}>
+          +
+        </button>
+        <button
+          className="button-subtract"
+          onClick={() => dispatch(decrement(2))}
+        >
+          -
+        </button>
 
         {isLogged ? <h3>Valuble information I shouldn't see</h3> : ""}
       </div>
